5a7fdee1cb7ad139724b2408dc0054f021c54790
buketmert <buketbahadir1@gmail.com>
2023-03-24 01:14:30 +0300

    implicit wait decreased

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -14,7 +14,7 @@ public class BasePage {
 	public RemoteWebDriver driver;
 	public static String browser;
 	public static int WAIT_TIME = 15;
-	public static int IMPLICITYLY_WAIT = 10;
+	public static int IMPLICITYLY_WAIT = 7;
 	private static int PAGE_LOAD_TIMEOUT = 30;
 
 	@Before
